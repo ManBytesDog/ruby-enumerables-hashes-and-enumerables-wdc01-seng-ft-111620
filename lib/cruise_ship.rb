9921b36e494207bc821b3b5f469ccd510028bd5a
T.J. Marstiller <thomasj.marstiller@yahoo.com>
2020-09-27 17:04:40 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,10 +8,9 @@
 
 def select_winner(passengers)
   winner = ""
-  passengers.each do |key, value|
+  passengers.each do |suite, name|
     if key == :suite_a && value.start_with?("A")
     winner = value
-  end
   end 
 end
 
